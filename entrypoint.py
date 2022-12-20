0e737bdc02765379c5a3c24c604f394d8b610e6f
Hugo Rabock <hugorabock@nasajon.com.br>
2022-12-20 17:03:20 -0300

    Service Document Worker - alterando processamento para ser emissao e importacao em paralelo

--- entrypoint.py
+++ entrypoint.py
@@ -1,9 +1,24 @@
 import os
 import time
+from multiprocessing import Process
+
+def call_emission_routine():
+    delay = os.getenv("frequencia_job", 120) # 2 minutos por padrão
+
+    while True:
+        os.system("python -m nsj_jobs.somente_emitir_nota")
+        time.sleep(delay)
 
 
-delay = os.getenv("frequencia_job", 300) # 5 minutos por padrão
+def call_import_routine():
+    delay = os.getenv("frequencia_job", 300) # 5 minutos por padrão
 
-while True:
-    os.system("python -m nsj_jobs.emissao_nota")
-    time.sleep(delay)
+    while True:
+        os.system("python -m nsj_jobs.somente_importar_nota")
+        time.sleep(delay)
+
+if __name__ == '__main__':
+    emission = Process(target=call_emission_routine)
+    importation = Process(target=call_import_routine)
+    importation.start()
+    emission.start()
